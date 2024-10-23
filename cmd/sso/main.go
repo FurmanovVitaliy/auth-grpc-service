02b2fbbffa4ecb4f2bb66dde8229042ffee56050
Vitalii Furmanov <furmanov.vitaliy@gmail.com>
2024-10-23 19:46:13 +0200

    initial commit

---
+++ cmd/sso/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"pix-clo-auth-service/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setUpLogger(cfg.Env)
+
+	log.Info("starting application")
+
+	//init app
+	//run grpc server
+
+}
+
+func setUpLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
